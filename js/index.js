6c169f66043779b33f5594c393d68afae802903a
Adrian <42228438+adrnlm@users.noreply.github.com>
2020-04-14 12:51:49 +1000

    login function

--- js/index.js
+++ js/index.js
@@ -8,6 +8,31 @@ signupForm.addEventListener("submit", (e) => {
   const password = signupForm["signup-password"].value;
 
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred);
+    console.log("User Registered");
+    signupForm.reset();
+  });
+});
+
+const logout = document.querySelector("#logout");
+
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("User Signed Out");
+  });
+});
+
+const signinForm = document.querySelector("#signin-form");
+
+signinForm.addEventListener("submit", (e) => {
+  //   prevent page refresh
+  e.preventDefault();
+
+  const email = signinForm["signin-email"].value;
+  const password = signinForm["signin-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log("User Signed In");
+    signinForm.reset();
   });
 });
