a997cc5dcbfd9cdc13e85393b68f618fc9007bb3
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-04-20 16:41:15 +1000

    Added the basic functionality

--- js/user-main-page.js
+++ js/user-main-page.js
@@ -1,6 +1,12 @@
+//checks to see if that user has logged in
 auth.onAuthStateChanged((user) => {
   if (user) {
     console.log(user.email, "has logged in");
+    //gets data
+    db.collection('users').doc(user.uid).get().then(doc => {
+      // TODO: add more functionality
+      console.log(doc.data().Title);
+    })
   } else {
     console.log("user has logged out");
   }
