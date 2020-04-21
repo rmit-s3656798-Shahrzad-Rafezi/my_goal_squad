4603ab4b2653e0fdc7a8ac741c842f2681fe3e94
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-04-21 11:02:25 +1000

    Added delete functionality

--- js/user-main-page.js
+++ js/user-main-page.js
@@ -1,17 +1,28 @@
 const todo_list = document.querySelector('#todo-list'); 
 const form = document.querySelector('#add-todo-form');
+let type = '';
 
 //create element and render
 function renderList(doc){
   let li = document.createElement('li');
   let todo = document.createElement('span');
+  let cross = document.createElement('div');
 
   li.setAttribute('data-id', doc.id);
   todo.textContent = doc.data().todo;
+  cross.textContent = 'x';
 
   li.appendChild(todo);
+  li.appendChild(cross);
 
   todo_list.appendChild(li);
+
+  // deleting data
+  cross.addEventListener('click', (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    type.doc(id).delete();
+  });
 }
 
 //checks to see if that user has logged in
@@ -21,11 +32,11 @@ auth.onAuthStateChanged((user) => {
     console.log(user.email, "has logged in");
     
     var user = db.collection('users').doc(user.uid);
-    var goals = user.collection('Goals').doc('Year');
-    var year = goals.collection('2020').doc('Month');
-    var month = year.collection('January').doc('Week');
-    var week = month.collection('Week1').doc('Type');
-    var type = week.collection("Personal");
+    let goals = user.collection('Goals').doc('Year');
+    let year = goals.collection('2020').doc('Month');
+    let month = year.collection('January').doc('Week');
+    let week = month.collection('Week1').doc('Type');
+    type = week.collection("Personal");
 
     //get data
     type.get().then((snapshot) =>{
