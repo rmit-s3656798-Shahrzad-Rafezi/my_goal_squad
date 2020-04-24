c47906ffd4239c5f6877bded5c9fb656bf77c35c
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-04-24 13:44:20 +1000

    Added select for year, month, week and type and its half functionality

--- js/user-main-page.js
+++ js/user-main-page.js
@@ -1,5 +1,69 @@
-const form = document.querySelector('#add-todo-form');
-let type = '';
+
+var y = document.getElementById('select_id_year');
+let chosen_year = '';
+y.addEventListener('change', function() {
+  if(this.value == '2020'){
+    chosen_year = this.value;
+  }
+  else if(this.value == '2021'){
+    chosen_year = this.value;
+  }
+  else if(this.value == '2022'){
+    chosen_year = this.value;
+  }
+  console.log(chosen_year);
+});
+
+var m = document.getElementById('select_id_month');
+let chosen_month = '';
+m.addEventListener('change', function() {
+  if(this.value == 'January'){
+    chosen_month = this.value;
+  }
+  else if(this.value == 'February'){
+    chosen_month = this.value;
+  }
+  else if(this.value == 'March'){
+    chosen_month = this.value;
+  }
+  console.log(chosen_month);
+});
+
+var w = document.getElementById('select_id_week');
+let chosen_week = '';
+w.addEventListener('change', function() {
+  if(this.value == 'Week1'){
+    chosen_week = this.value;
+  }
+  else if(this.value == 'Week2'){
+    chosen_week = this.value;
+  }
+  else if(this.value == 'Week3'){
+    chosen_week = this.value;
+  }
+  console.log(chosen_week);
+});
+
+var t = document.getElementById('select_id_type');
+let chosen_type = '';
+t.addEventListener('change', function() {
+  if(this.value == 'Health'){
+    chosen_type = this.value;
+  }
+  else if(this.value == 'Work'){
+    chosen_type = this.value;
+  }
+  else if(this.value == 'Personal'){
+    chosen_type = this.value;
+  }
+  console.log(chosen_type);
+});
+
+// This is for selection element in HTML file
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, {});
+});
 
 // enable offline data
 db.enablePersistence().catch(function(err) {
@@ -20,7 +84,7 @@ const renderList = (data, id) => {
   const html = `
   <div class="card-panel todo white row" data-id="${id}">
       <div class="todo-details">
-        <div class="recipe-title">${data.todo}</div>
+        <div>${data.todo}</div>
       </div>
       <div class="todo-delete">
         <i class="material-icons" data-id="${id}">delete_outline</i>
@@ -53,38 +117,56 @@ auth.onAuthStateChanged((user) => {
 
     console.log(user.email, "has logged in");
     
-    var user = db.collection('users').doc(user.uid);
-    var goals = user.collection('Goals').doc('Year');
-    var year = goals.collection('2020').doc('Month');
-    var month = year.collection('January').doc('Week');
-    var week = month.collection('Week1').doc('Type');
-    type = week.collection("Personal");
-
     // Get data in real-time
-    type.onSnapshot(snapshot => {
-
+    db.collection('users').doc(user.uid)
+    .collection('Goals').doc('Year')
+    .collection('2020').doc('Month')
+    .collection('January').doc('Week')
+    .collection('Week1').doc('Type')
+    .collection('Personal').onSnapshot(snapshot =>{
       let changes = snapshot.docChanges();
-
       changes.forEach(change =>{
-        
         if(change.type === 'added'){
           renderList(change.doc.data(), change.doc.id);
         }
-
         else if(change.type === 'removed'){
           removeList(change.doc.id);
         }
-
       });
+    });
 
+    db.collection('users').doc(user.uid)
+    .collection('Goals').doc('Year')
+    .collection('2020').doc('Month')
+    .collection('February').doc('Week')
+    .collection('Week1').doc('Type')
+    .collection('Personal').onSnapshot(snapshot =>{
+      let changes = snapshot.docChanges();
+      changes.forEach(change =>{
+        if(change.type === 'added'){
+          renderList(change.doc.data(), change.doc.id);
+        }
+        else if(change.type === 'removed'){
+          removeList(change.doc.id);
+        }
+      });
     });
 
     // Add todo list data
+    const form = document.querySelector('#todo-form');
     form.addEventListener('submit', (e) =>{
+
       e.preventDefault();
-      type.add({
+
+      db.collection('users').doc(user.uid)
+      .collection('Goals').doc('Year')
+      .collection(chosen_year).doc('Month')
+      .collection(chosen_month).doc('Week')
+      .collection(chosen_week).doc('Type')
+      .collection(chosen_type).add({
         todo: form.todo.value
       });
+
       form.todo.value = '';
     });
 
