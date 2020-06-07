05dd66ee1bb81dabc66bb3e50032ae9649ac25da
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-06-07 12:32:38 +1000

    Service worker is now done

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static';
-// const dynamicCacheName = 'site-dynamic-v7';
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v2';
 const assets = [
   '/',
   '/index.html',
@@ -25,19 +25,21 @@ const assets = [
   '/css/quote-of-the-day.css',
   '/css/user-profile-page.css',
   '/img/logo.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  '/img/logo.svg',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
-// // cache size limit function
-// const limitCacheSize = (name, size) => {
-//   caches.open(name).then(cache => {
-//     cache.keys().then(keys => {
-//       if(keys.length > size){
-//         cache.delete(keys[0]).then(limitCacheSize(name, size));
-//       }
-//     });
-//   });
-// };
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 // install event
 self.addEventListener('install', evt => {
@@ -50,39 +52,42 @@ self.addEventListener('install', evt => {
    );
 });
 
-// // activate event
+// activate event
 self.addEventListener('activate', evt => {
   console.log('service worker activated');
-  // evt.waitUntil(
-  //   caches.keys().then(keys => {
-  //     //console.log(keys);
-  //     return Promise.all(keys
-  //       .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-  //       .map(key => caches.delete(key))
-  //     );
-  //   })
-  // );
+  evt.waitUntil(
+     caches.keys().then(keys => {
+       //console.log(keys);
+       return Promise.all(keys
+         .filter(key => key !== staticCacheName)
+         .map(key => caches.delete(key))
+       );
+     })
+   );
 });
 
-// // fetch events
-// self.addEventListener('fetch', evt => {
+// NOTE: if you want to change something to the design or add extra code, 
+// please comment this as we don't want cache it.
+
+// fetch events
+self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
-//   if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
-//     evt.respondWith(
-//       caches.match(evt.request).then(cacheRes => {
-//         return cacheRes || fetch(evt.request).then(fetchRes => {
-//           return caches.open(dynamicCacheName).then(cache => {
-//             cache.put(evt.request.url, fetchRes.clone());
-//             // check cached items size
-//             limitCacheSize(dynamicCacheName, 15);
-//             return fetchRes;
-//           })
-//         });
-//       }).catch(() => {
-//         if(evt.request.url.indexOf('.html') > -1){
-//           return caches.match('/pages/fallback.html');
-//         }
-//       })
-//     );
-//   }
-// });
+  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+     evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+         return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+      //   if(evt.request.url.indexOf('.html') > -1){
+      //     return caches.match('/pages/fallback.html');
+      //   }
+       })
+    );
+  }
+});
