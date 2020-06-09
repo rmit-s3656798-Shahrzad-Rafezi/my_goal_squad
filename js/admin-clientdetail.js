349c2d95d8e8ca482f40773b3c3b8eccc0857a40
jiecai ma <s3653360@student.rmit.edu.au>
2020-06-09 15:15:55 +1000

    Separate customer profile page

---
+++ js/admin-clientdetail.js
@@ -0,0 +1,503 @@
+// listen for auth changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user.email, "has logged in");
+    for (var i = 0; i <= months.length - 1; i++) {
+      for (var j = 0; j <= weeks.length - 1; j++) {
+        for (var k = 0; k <= types.length - 1; k++) {
+          display(current_year.toString(), months[i], weeks[j], types[k]);
+        }
+      }
+    }
+  } else {
+    console.log("user has logged out");
+  }
+});
+
+// Register form
+const signupForm = document.querySelector("#signup-form");
+
+
+// Logout Button
+const logout = document.querySelector("#logout-btn");
+
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    window.location.href = "../index.html";
+  });
+});
+
+// Make Admin
+const adminForm = document.querySelector("#make-admin");
+
+// Modal Triggers
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+// Upload Quote to Firebase Storage
+const progressBar = document.getElementById('pgb')
+const file_form = document.querySelector('#file-upload-form');
+const file_upload = document.getElementById('upload-file-btn');
+
+
+
+// Random Number Generator
+function generateRandomInteger(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min))
+}
+
+// Delete Random Quote when limit is reached
+function deleteRandomQuote(limit) {
+  let x = 0;
+  var storageRef = firebase.storage().ref("quotes");
+  storageRef.listAll().then((result) => {
+    result.items.forEach((item) => {
+      x += 1;
+    });
+    if (x >= limit) {
+
+      var file = result.items[generateRandomInteger(0, 2)];
+
+      file.getMetadata().then((data) => {
+        // Create a reference to the file to delete
+        var earliestFile = storageRef.child(data.name);
+
+        // Delete the file
+        earliestFile.delete().then(function () {
+          // File deleted successfully
+          console.log("File Deleted Successfully")
+          return true;
+        }).catch(function (error) {
+          // Uh-oh, an error occurred!
+          console.log(error)
+        });
+      })
+    };
+  });
+
+}
+
+// This is for selection, tabs, modal element in HTML file (CSS Materialize)
+document.addEventListener('DOMContentLoaded', function () {
+  var select = document.querySelectorAll('select');
+  M.FormSelect.init(select, {});
+
+  
+  const options = {
+    duration: 300,
+    onShow: null,
+    swipeable: true,
+    responsiveThreshold: Infinity
+  };
+
+  const tabsContainer = document.querySelector("#tabs-swipe-demo");
+  M.Tabs.init(tabsContainer, options);
+
+  var modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+});
+
+// Display Years
+var year_id = document.getElementById('select_id_year');
+year_Fragment = document.createDocumentFragment();
+let chosen_year = '';
+
+let years = new Array(3);
+let current_year = new Date().getFullYear();
+
+for (i = 0; i <= 3; i++) {
+  years[i] = current_year + i;
+}
+
+for (var i = 0; i <= years.length - 1; i++) {
+  var option = document.createElement('option');
+  option.value = years[i];
+  option.appendChild(document.createTextNode(years[i]));
+  year_Fragment.appendChild(option);
+}
+year_id.appendChild(year_Fragment);
+
+// Grabs the year value
+year_id.addEventListener('change', function () {
+  chosen_year = this.value;
+});
+
+// Display Months
+var month_id = document.getElementById('select_id_month');
+month_Fragment = document.createDocumentFragment();
+let chosen_month = '';
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+for (var i = 0; i <= months.length - 1; i++) {
+  var option = document.createElement('option');
+  option.value = months[i];
+  option.appendChild(document.createTextNode(months[i]));
+  month_Fragment.appendChild(option);
+}
+month_id.appendChild(month_Fragment);
+
+// Grabs the Month value
+month_id.addEventListener('change', function () {
+  chosen_month = this.value;
+});
+
+
+// Display Weeks
+var week_id = document.getElementById('select_id_week');
+week_fragment = document.createDocumentFragment();
+let chosen_week = '';
+
+var weeks = ["Week1", "Week2", "Week3", "Week4"];
+
+for (var i = 0; i <= weeks.length - 1; i++) {
+  var option = document.createElement('option');
+  option.value = weeks[i];
+  option.appendChild(document.createTextNode(weeks[i]));
+  week_fragment.appendChild(option);
+}
+week_id.appendChild(week_fragment);
+
+// Grabs the Week value
+week_id.addEventListener('change', function () {
+  chosen_week = this.value;
+});
+
+// Display Types
+var type_id = document.getElementById('select_id_type');
+type_fragment = document.createDocumentFragment();
+let chosen_type = '';
+
+var types = ["Health", "Career", "Personal", "Financial", "Other"];
+
+for (var i = 0; i <= types.length - 1; i++) {
+  var option = document.createElement('option');
+  option.value = types[i];
+  option.appendChild(document.createTextNode(types[i]));
+  type_fragment.appendChild(option);
+}
+type_id.appendChild(type_fragment);
+
+
+// Grabs the Type value
+type_id.addEventListener('change', function () {
+  chosen_type = this.value;
+});
+
+
+// Add todo list data
+const form = document.querySelector('#todo-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('users').doc(chosen_user.value)
+    .collection('Goals').doc('Year')
+    .collection(chosen_year).doc('Month')
+    .collection(chosen_month).doc('Week')
+    .collection(chosen_week).doc('Type')
+    .collection(chosen_type).add({
+      todo: form.todo.value,
+      range: 0
+    });
+  form.todo.value = '';
+});
+
+//Confirm messsage
+var Confirmyear = document.getElementById("select_id_year");
+var Confirmmonth = document.getElementById("select_id_month");
+var Confirmweek = document.getElementById("select_id_week");
+var Confirmtype = document.getElementById("select_id_type");
+var Confirmtodo = document.getElementById("todo");
+
+function goalconfirm() {
+  document.getElementById("goal-confirm").innerHTML =
+    chosen_user.value + " " +
+    Confirmyear.value + " " +
+    Confirmmonth.value + " " +
+    Confirmweek.value + " " +
+    Confirmtype.value;
+  document.getElementById("goal-confirm-text").innerHTML = Confirmtodo.value;
+}
+
+// get userlist
+db.collection("users").get().then(snapshot => {
+  snapshot.forEach(doc => {
+    const user_tab = document.querySelector('#select_id_user');
+    const html = `
+      <option value="${doc.id}">${doc.data().displayName}</option>
+    `;
+    user_tab.innerHTML += html;
+  });
+});
+
+var chosen_user = document.getElementById("userlist");
+
+// Get data in real-time
+function display(year, month, week, type) {
+
+  const removeList = (id) => {
+    const todo = document.querySelector(`.todo[data-id="${id}"]`);
+    todo.remove();
+  };
+
+  if (type == "Health") {
+    db.collection('users').doc("2ISIor1btaNuRthT3Z8jdYIcLKl2")
+      .collection('Goals').doc('Year')
+      .collection(year).doc('Month')
+      .collection(month).doc('Week')
+      .collection(week).doc('Type')
+      .collection(type).onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+          if (change.type === 'added') {
+            console.log(change.doc.data())
+            renderHealth(change.doc.data(), change.doc.id);
+          }
+          else if (change.type === 'removed') {
+            removeList(change.doc.id);
+          }
+        });
+      });
+  }
+
+  if (type == "Career") {
+    db.collection('users').doc("2ISIor1btaNuRthT3Z8jdYIcLKl2")
+      .collection('Goals').doc('Year')
+      .collection(year).doc('Month')
+      .collection(month).doc('Week')
+      .collection(week).doc('Type')
+      .collection(type).onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+          if (change.type === 'added') {
+            renderCareer(change.doc.data(), change.doc.id);
+          }
+          else if (change.type === 'removed') {
+            removeList(change.doc.id);
+          }
+        });
+      });
+  }
+
+  if (type == "Personal") {
+    db.collection('users').doc("2ISIor1btaNuRthT3Z8jdYIcLKl2")
+      .collection('Goals').doc('Year')
+      .collection(year).doc('Month')
+      .collection(month).doc('Week')
+      .collection(week).doc('Type')
+      .collection(type).onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+          if (change.type === 'added') {
+            renderPersonal(change.doc.data(), change.doc.id);
+          }
+          else if (change.type === 'removed') {
+            removeList(change.doc.id);
+          }
+        });
+      });
+  }
+
+  if (type == "Financial") {
+    db.collection('users').doc("2ISIor1btaNuRthT3Z8jdYIcLKl2")
+      .collection('Goals').doc('Year')
+      .collection(year).doc('Month')
+      .collection(month).doc('Week')
+      .collection(week).doc('Type')
+      .collection(type).onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+          if (change.type === 'added') {
+            renderFinancial(change.doc.data(), change.doc.id);
+          }
+          else if (change.type === 'removed') {
+            removeList(change.doc.id);
+          }
+        });
+      });
+  }
+
+  if (type == "Other") {
+    db.collection('users').doc("2ISIor1btaNuRthT3Z8jdYIcLKl2")
+      .collection('Goals').doc('Year')
+      .collection(year).doc('Month')
+      .collection(month).doc('Week')
+      .collection(week).doc('Type')
+      .collection(type).onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+          if (change.type === 'added') {
+            renderOther(change.doc.data(), change.doc.id);
+          }
+          else if (change.type === 'removed') {
+            removeList(change.doc.id);
+          }
+        });
+      });
+  }
+}
+const health_tab = document.querySelector('#test-swipe-1');
+const renderHealth = (data, id) => {
+  const html = `
+    <div class="card-panel todo white row" data-id="${id}">
+        <div class="todo-details">
+          <div>${data.todo}</div>
+        </div>
+        <div class="todo-delete">
+          <i class="material-icons" data-id="${id}">delete_outline</i>
+        </div>
+    </div>
+    `;
+  health_tab.innerHTML += html;
+};
+
+const career_tab = document.querySelector('#test-swipe-2');
+const renderCareer = (data, id) => {
+  const html = `
+    <div class="card-panel todo white row" data-id="${id}">
+        <div class="todo-details">
+          <div>${data.todo}</div>
+        </div>
+        <div class="todo-delete">
+          <i class="material-icons" data-id="${id}">delete_outline</i>
+        </div>
+    </div>
+    `;
+  career_tab.innerHTML += html;
+};
+
+const personal_tab = document.querySelector('#test-swipe-3');
+const renderPersonal = (data, id) => {
+  const html = `
+    <div class="card-panel todo white row" data-id="${id}">
+        <div class="todo-details">
+          <div>${data.todo}</div>
+        </div>
+        <div class="todo-delete">
+          <i class="material-icons" data-id="${id}">delete_outline</i>
+        </div>
+    </div>
+    `;
+  personal_tab.innerHTML += html;
+};
+
+const financial_tab = document.querySelector('#test-swipe-4');
+const renderFinancial = (data, id) => {
+  const html = `
+    <div class="card-panel todo white row" data-id="${id}">
+        <div class="todo-details">
+          <div>${data.todo}</div>
+        </div>
+        <div class="todo-delete">
+          <i class="material-icons" data-id="${id}">delete_outline</i>
+        </div>
+    </div>
+    `;
+  financial_tab.innerHTML += html;
+};
+
+const other_tab = document.querySelector('#test-swipe-5');
+const renderOther = (data, id) => {
+  const html = `
+    <div class="card-panel todo white row" data-id="${id}">
+        <div class="todo-details">
+          <div>${data.todo}</div>
+        </div>
+        <div class="todo-delete">
+          <i class="material-icons" data-id="${id}">delete_outline</i>
+        </div>
+    </div>
+    `;
+  other_tab.innerHTML += html;
+};
+
+const healthContainer = document.querySelector('#test-swipe-1');
+healthContainer.addEventListener('click', e => {
+  if (e.target.tagName === 'I') {
+    const id = e.target.getAttribute('data-id');
+    // Delete goals dynamically
+    for (var i = 0; i <= months.length - 1; i++) {
+      for (var j = 0; j <= weeks.length - 1; j++) {
+        for (var k = 0; k <= types.length - 1; k++) {
+          delete_todo_list("2ISIor1btaNuRthT3Z8jdYIcLKl2", current_year.toString(), months[i], weeks[j], types[k], id);
+        }
+      }
+    }
+  }
+});
+
+const careerContainer = document.querySelector('#test-swipe-2');
+careerContainer.addEventListener('click', e => {
+  if (e.target.tagName === 'I') {
+    const id = e.target.getAttribute('data-id');
+    // Delete goals dynamically
+    for (var i = 0; i <= months.length - 1; i++) {
+      for (var j = 0; j <= weeks.length - 1; j++) {
+        for (var k = 0; k <= types.length - 1; k++) {
+          delete_todo_list(user.uid, current_year.toString(), months[i], weeks[j], types[k], id);
+        }
+      }
+    }
+  }
+});
+
+const personalContainer = document.querySelector('#test-swipe-3');
+personalContainer.addEventListener('click', e => {
+  if (e.target.tagName === 'I') {
+    const id = e.target.getAttribute('data-id');
+    // Delete goals dynamically
+    for (var i = 0; i <= months.length - 1; i++) {
+      for (var j = 0; j <= weeks.length - 1; j++) {
+        for (var k = 0; k <= types.length - 1; k++) {
+          delete_todo_list(user.uid, current_year.toString(), months[i], weeks[j], types[k], id);
+        }
+      }
+    }
+  }
+});
+
+const financialContainer = document.querySelector('#test-swipe-4');
+financialContainer.addEventListener('click', e => {
+  if (e.target.tagName === 'I') {
+    const id = e.target.getAttribute('data-id');
+    // Delete goals dynamically
+    for (var i = 0; i <= months.length - 1; i++) {
+      for (var j = 0; j <= weeks.length - 1; j++) {
+        for (var k = 0; k <= types.length - 1; k++) {
+          delete_todo_list(user.uid, current_year.toString(), months[i], weeks[j], types[k], id);
+        }
+      }
+    }
+  }
+});
+
+const otherContainer = document.querySelector('#test-swipe-5');
+otherContainer.addEventListener('click', e => {
+  if (e.target.tagName === 'I') {
+    const id = e.target.getAttribute('data-id');
+    // Delete goals dynamically
+    for (var i = 0; i <= months.length - 1; i++) {
+      for (var j = 0; j <= weeks.length - 1; j++) {
+        for (var k = 0; k <= types.length - 1; k++) {
+          delete_todo_list(user.uid, current_year.toString(), months[i], weeks[j], types[k], id);
+        }
+      }
+    }
+  }
+});
+
+function delete_todo_list(userID, year, month, week, type, docID) {
+  db.collection('users').doc(userID)
+    .collection('Goals').doc('Year')
+    .collection(year).doc('Month')
+    .collection(month).doc('Week')
+    .collection(week).doc('Type')
+    .collection(type).doc(docID).delete();
+}
+
+function selectuser(){
+  var clientid = document.getElementById("userlist"). value;
+  console.log(clientid);
+}
