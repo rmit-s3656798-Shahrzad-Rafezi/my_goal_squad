c4a606a9ff15f79ed7a11e81d41425786ee09725
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2020-06-11 11:10:42 +1000

    Added colour and range to the cards
    Note: need to add the modal for update functionality

--- js/admin-clientdetail.js
+++ js/admin-clientdetail.js
@@ -7,8 +7,6 @@ auth.onAuthStateChanged((user) => {
   }
 });
 
-
-
 // Logout Button
 const logout = document.querySelector("#logout-btn");
 
@@ -19,26 +17,21 @@ logout.addEventListener("click", (e) => {
   });
 });
 
-
 // Modal Triggers
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
 });
 
-
-
 // Random Number Generator
 function generateRandomInteger(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min))
 }
 
-
 // This is for selection, tabs, modal element in HTML file (CSS Materialize)
 document.addEventListener('DOMContentLoaded', function () {
   var select = document.querySelectorAll('select');
   M.FormSelect.init(select, {});
-
 
   const options = {
     duration: 300,
@@ -99,7 +92,6 @@ month_id.addEventListener('change', function () {
   chosen_month = this.value;
 });
 
-
 // Display Weeks
 var week_id = document.getElementById('select_id_week');
 week_fragment = document.createDocumentFragment();
@@ -136,12 +128,10 @@ for (var i = 0; i <= types.length - 1; i++) {
 }
 type_id.appendChild(type_fragment);
 
-
 // Grabs the Type value
 type_id.addEventListener('change', function () {
   chosen_type = this.value;
 });
-
 
 // Add todo list data
 const form = document.querySelector('#todo-form');
@@ -193,15 +183,10 @@ db.collection("users").get().then(snapshot => {
 var chosen_user = document.getElementById("userlist");
 
 // Get data in real-time
-function display(clientid, year, month, week, type) {
-
-  const removeList = (id) => {
-    const todo = document.querySelector(`.todo[data-id="${id}"]`);
-    todo.remove();
-  };
+function display(userID, year, month, week, type) {
 
   if (type == "Health") {
-    db.collection('users').doc(clientid)
+    db.collection('users').doc(userID)
       .collection('Goals').doc('Year')
       .collection(year).doc('Month')
       .collection(month).doc('Week')
@@ -212,6 +197,11 @@ function display(clientid, year, month, week, type) {
           if (change.type === 'added') {
             renderHealth(change.doc.data(), change.doc.id);
           }
+          // Update range and then render
+          else if (change.type === 'modified') {
+            renderRange(change.doc.data(), change.doc.id);
+            render_new_goal(change.doc.data(), change.doc.id);
+          }
           else if (change.type === 'removed') {
             removeList(change.doc.id);
           }
@@ -220,7 +210,7 @@ function display(clientid, year, month, week, type) {
   }
 
   if (type == "Career") {
-    db.collection('users').doc(clientid)
+    db.collection('users').doc(userID)
       .collection('Goals').doc('Year')
       .collection(year).doc('Month')
       .collection(month).doc('Week')
@@ -231,6 +221,11 @@ function display(clientid, year, month, week, type) {
           if (change.type === 'added') {
             renderCareer(change.doc.data(), change.doc.id);
           }
+          // Update range and then render
+          else if (change.type === 'modified') {
+            renderRange(change.doc.data(), change.doc.id);
+            render_new_goal(change.doc.data(), change.doc.id);
+          }
           else if (change.type === 'removed') {
             removeList(change.doc.id);
           }
@@ -239,7 +234,7 @@ function display(clientid, year, month, week, type) {
   }
 
   if (type == "Personal") {
-    db.collection('users').doc(clientid)
+    db.collection('users').doc(userID)
       .collection('Goals').doc('Year')
       .collection(year).doc('Month')
       .collection(month).doc('Week')
@@ -250,6 +245,11 @@ function display(clientid, year, month, week, type) {
           if (change.type === 'added') {
             renderPersonal(change.doc.data(), change.doc.id);
           }
+          // Update range and then render
+          else if (change.type === 'modified') {
+            renderRange(change.doc.data(), change.doc.id);
+            render_new_goal(change.doc.data(), change.doc.id);
+          }
           else if (change.type === 'removed') {
             removeList(change.doc.id);
           }
@@ -258,7 +258,7 @@ function display(clientid, year, month, week, type) {
   }
 
   if (type == "Financial") {
-    db.collection('users').doc(clientid)
+    db.collection('users').doc(userID)
       .collection('Goals').doc('Year')
       .collection(year).doc('Month')
       .collection(month).doc('Week')
@@ -269,6 +269,11 @@ function display(clientid, year, month, week, type) {
           if (change.type === 'added') {
             renderFinancial(change.doc.data(), change.doc.id);
           }
+          // Update range and then render
+          else if (change.type === 'modified') {
+            renderRange(change.doc.data(), change.doc.id);
+            render_new_goal(change.doc.data(), change.doc.id);
+          }
           else if (change.type === 'removed') {
             removeList(change.doc.id);
           }
@@ -277,7 +282,7 @@ function display(clientid, year, month, week, type) {
   }
 
   if (type == "Other") {
-    db.collection('users').doc(clientid)
+    db.collection('users').doc(userID)
       .collection('Goals').doc('Year')
       .collection(year).doc('Month')
       .collection(month).doc('Week')
@@ -287,6 +292,11 @@ function display(clientid, year, month, week, type) {
         changes.forEach(change => {
           if (change.type === 'added') {
             renderOther(change.doc.data(), change.doc.id);
+          }
+          // Update range and then render
+          else if (change.type === 'modified') {
+            renderRange(change.doc.data(), change.doc.id);
+            render_new_goal(change.doc.data(), change.doc.id);
           }
           else if (change.type === 'removed') {
             removeList(change.doc.id);
@@ -298,9 +308,10 @@ function display(clientid, year, month, week, type) {
 const health_tab = document.querySelector('#test-swipe-1');
 const renderHealth = (data, id) => {
   const html = `
-    <div class="card-panel todo white row" data-id="${id}">
+    <div id="change_colour" class="card-panel todo row" data-id="${id}">
         <div class="todo-details">
-          <div>${data.todo}</div>
+          <a data-id="${id}" data-target="modal1" class="modal-trigger">${data.todo}</a>
+          <p class="update_range" data-id="${id}">${data.range}</p>        
         </div>
         <div class="todo-delete">
           <i class="material-icons" data-id="${id}">delete_outline</i>
@@ -308,14 +319,17 @@ const renderHealth = (data, id) => {
     </div>
     `;
   health_tab.innerHTML += html;
+  //Show red, orange and green based on the range
+  changeColour(data.range, id);
 };
 
 const career_tab = document.querySelector('#test-swipe-2');
 const renderCareer = (data, id) => {
   const html = `
-    <div class="card-panel todo white row" data-id="${id}">
+    <div id="change_colour" class="card-panel todo row" data-id="${id}">
         <div class="todo-details">
-          <div>${data.todo}</div>
+          <a data-id="${id}" data-target="modal1" class="modal-trigger">${data.todo}</a>
+          <p class="update_range" data-id="${id}">${data.range}</p>        
         </div>
         <div class="todo-delete">
           <i class="material-icons" data-id="${id}">delete_outline</i>
@@ -323,14 +337,17 @@ const renderCareer = (data, id) => {
     </div>
     `;
   career_tab.innerHTML += html;
+  //Show red, orange and green based on the range
+  changeColour(data.range, id);
 };
 
 const personal_tab = document.querySelector('#test-swipe-3');
 const renderPersonal = (data, id) => {
   const html = `
-    <div class="card-panel todo white row" data-id="${id}">
+    <div id="change_colour" class="card-panel todo row" data-id="${id}">
         <div class="todo-details">
-          <div>${data.todo}</div>
+          <a data-id="${id}" data-target="modal1" class="modal-trigger">${data.todo}</a>
+          <p class="update_range" data-id="${id}">${data.range}</p>        
         </div>
         <div class="todo-delete">
           <i class="material-icons" data-id="${id}">delete_outline</i>
@@ -338,14 +355,17 @@ const renderPersonal = (data, id) => {
     </div>
     `;
   personal_tab.innerHTML += html;
+  //Show red, orange and green based on the range
+  changeColour(data.range, id);
 };
 
 const financial_tab = document.querySelector('#test-swipe-4');
 const renderFinancial = (data, id) => {
   const html = `
-    <div class="card-panel todo white row" data-id="${id}">
+    <div id="change_colour" class="card-panel todo row" data-id="${id}">
         <div class="todo-details">
-          <div>${data.todo}</div>
+          <a data-id="${id}" data-target="modal1" class="modal-trigger">${data.todo}</a>
+          <p class="update_range" data-id="${id}">${data.range}</p>        
         </div>
         <div class="todo-delete">
           <i class="material-icons" data-id="${id}">delete_outline</i>
@@ -353,14 +373,17 @@ const renderFinancial = (data, id) => {
     </div>
     `;
   financial_tab.innerHTML += html;
+  //Show red, orange and green based on the range
+  changeColour(data.range, id);
 };
 
 const other_tab = document.querySelector('#test-swipe-5');
 const renderOther = (data, id) => {
   const html = `
-    <div class="card-panel todo white row" data-id="${id}">
+    <div id="change_colour" class="card-panel todo row" data-id="${id}">
         <div class="todo-details">
-          <div>${data.todo}</div>
+          <a data-id="${id}" data-target="modal1" class="modal-trigger">${data.todo}</a>
+          <p class="update_range" data-id="${id}">${data.range}</p>        
         </div>
         <div class="todo-delete">
           <i class="material-icons" data-id="${id}">delete_outline</i>
@@ -368,7 +391,47 @@ const renderOther = (data, id) => {
     </div>
     `;
   other_tab.innerHTML += html;
+  //Show red, orange and green based on the range
+  changeColour(data.range, id);
 };
+
+// Remove list from DOM
+const removeList = (id) => {
+  const todo = document.querySelector(`.todo[data-id="${id}"]`);
+  todo.remove();
+};
+
+function changeColour(range_value, id_value) {
+  var change_colours = ["showRed", "showOrange", "showYellow", "showGreen"];
+
+  if (range_value < 25) {
+    //Show red
+    for (var i = 0; i <= change_colours.length; i++) {
+      $('#change_colour[data-id="' + id_value + '"]').removeClass(change_colours[i]);
+      $('#change_colour[data-id="' + id_value + '"]').addClass('showRed');
+      $('a[data-id="' + id_value + '"]').addClass('whiteText');
+    }
+  }
+
+  if (range_value > 25) {
+    //show orange
+    for (var i = 0; i <= change_colours.length; i++) {
+      $('#change_colour[data-id="' + id_value + '"]').removeClass(change_colours[i]);
+      $('#change_colour[data-id="' + id_value + '"]').addClass('showOrange');
+      $('a[data-id="' + id_value + '"]').addClass('whiteText');
+    }
+  }
+
+  if (range_value > 95) {
+    //show green
+    for (var i = 0; i <= change_colours.length; i++) {
+      $('#change_colour[data-id="' + id_value + '"]').removeClass(change_colours[i]);
+      $('#change_colour[data-id="' + id_value + '"]').addClass('showGreen');
+      $('a[data-id="' + id_value + '"]').addClass('whiteText');
+    }
+  }
+
+}
 
 const healthContainer = document.querySelector('#test-swipe-1');
 healthContainer.addEventListener('click', e => {
@@ -454,19 +517,19 @@ function delete_todo_list(chosen_client, year, month, week, type, docID) {
     .collection(type).doc(docID).delete();
 }
 
-  var clientid = document.getElementById("userlist");
+var clientid = document.getElementById("userlist");
 
-  let chosen_client = '';
-  
-  // Grabs the client value
-  clientid.addEventListener('change', function () {
-    chosen_client = this.value;
-    console.log(chosen_client);
-    for (var i = 0; i <= months.length - 1; i++) {
-      for (var j = 0; j <= weeks.length - 1; j++) {
-        for (var k = 0; k <= types.length - 1; k++) {
-          display(chosen_client, current_year.toString(), months[i], weeks[j], types[k]);
-        }
+let chosen_client = '';
+
+// Grabs the client value
+clientid.addEventListener('change', function () {
+  chosen_client = this.value;
+  console.log(chosen_client);
+  for (var i = 0; i <= months.length - 1; i++) {
+    for (var j = 0; j <= weeks.length - 1; j++) {
+      for (var k = 0; k <= types.length - 1; k++) {
+        display(chosen_client, current_year.toString(), months[i], weeks[j], types[k]);
       }
     }
-  });
+  }
+});
